b2efdecae6480af33c2cf068637740518d9834a3
André Ferreira <mrawesomeferreira@gmail.com>
2020-03-06 11:40:44 +0000

    workin on user

--- routes/survey.js
+++ routes/survey.js
@@ -9,8 +9,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-<<<<<<< HEAD
-  const { Gender, Country, Age, Wage, Sector, Education, Role, Perception } = req.body;
+  const { Gender, Country, Age, Wage, Sector, Education, Managerial, Perception } = req.body;
 
   User.create({
     Gender,
@@ -19,20 +18,13 @@ router.post('/', (req, res, next) => {
     Wage,
     Sector,
     Education,
-    Role,
+    Managerial,
     Perception
   })
     .then(newUser => {
       res.redirect(`/result/${newUser._id}`);
     })
     .catch(error => next(error));
-=======
-  User.create()
-
-
-
-  //res.redirect(`/result${._id}`, { info });
->>>>>>> 6df213a4b2f27eefcd84f4eac3ef8ca11a301a38
 });
 
 router.get('/result/:id', (req, res, next) => {
