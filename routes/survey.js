a1da3cb2615e2587741a19e34c4a69e6fc838e49
Gabriela Fonseca <gabmfonseca@gmail.com>
2020-03-06 11:42:55 +0000

    changed form

--- routes/survey.js
+++ routes/survey.js
@@ -9,30 +9,11 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-<<<<<<< HEAD
-  const { Gender, Country, Age, Wage, Sector, Education, Role, Perception } = req.body;
-
-  User.create({
-    Gender,
-    Country,
-    Age,
-    Wage,
-    Sector,
-    Education,
-    Role,
-    Perception
-  })
-    .then(newUser => {
-      res.redirect(`/result/${newUser._id}`);
-    })
-    .catch(error => next(error));
-=======
   User.create()
 
 
 
   //res.redirect(`/result${._id}`, { info });
->>>>>>> 6df213a4b2f27eefcd84f4eac3ef8ca11a301a38
 });
 
 router.get('/result/:id', (req, res, next) => {
